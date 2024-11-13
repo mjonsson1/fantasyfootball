2578830c2b8b08e69da8590c9bffe69318d74da1
Marco Jonsson <113134045+mjonsson1@users.noreply.github.com>
2024-11-13 11:20:36 -0600

    Revert "Ryan changes and Marco pushes to textfiles"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import RosterManagement from './RosterManagement';
-import InjuryImpactAnalyzer from './InjuryImpactAnalyzer';
-import Header from './Header';
-import './styles.css';
-import Standings from './Standings';
-import Login from'./Login';
-import WeeklyChallenges from './WeeklyChallenges';
+
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/roster" element={<RosterManagement />} />
-        <Route path="/analyzer" element={<InjuryImpactAnalyzer />} />
-        <Route path="/standings" element={<Standings />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/challenges" element ={<WeeklyChallenges />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Welcome to the Fantasy Football App!</h1>
+    </div>
   );
 }
 
